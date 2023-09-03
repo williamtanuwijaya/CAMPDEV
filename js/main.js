4dabe3e327ad237f0d6b130308738bb918cd678c
williamtanuwijaya <will.tw0807@gmail.com>
2023-09-03 09:22:29 +0700

    ak buat faq dan tolong perbaiki bug ketika klik icon plus

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 });
 
+// show/hide faq answer
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    // change icon
+    const icon = faq.querySelector('.faq_icon i');
+
+    if (icon.getAttribute('data-feather') === 'plus') {
+      icon.setAttribute('data-feather', 'minus');
+    } else {
+      icon.setAttribute('data-feather', 'plus');
+    }
+
+    feather.replace();
+  });
+});
