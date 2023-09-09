1891569a4bbcbf93978e0451243dec95735ccf60
Umar Muhdhor <muhdhorcs@gmail.com>
2023-09-09 08:22:53 +0700

    testimonial swiper blom jadi

--- src/sections/index/testimonial/Testimonial.jsx
+++ src/sections/index/testimonial/Testimonial.jsx
@@ -1,17 +1,109 @@
 import React from 'react'
+import './testimonial.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
 import avatar1 from "./../../../assets/images/avatar1.jpg"
 import avatar2 from "./../../../assets/images/avatar2.jpg"
 import avatar3 from "./../../../assets/images/avatar3.jpg"
 import avatar4 from "./../../../assets/images/avatar4.jpg"
 import avatar5 from "./../../../assets/images/avatar5.jpg"
-import './testimonial.css'
 
 const Testimonial = () => {
     return (
         <section className="container testimonials_container mySwiper">
             <h2>Students's Testimonials</h2>
-            <div className="swiper-wrapper">
-                <article className="testimonial swiper-slide">
+            <Swiper
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <article className="testimonial swiper-slide">
+                        <div className="avatar">
+                            <img src={avatar1} alt="" srcSet="" />
+                        </div>
+                        <div className="testimonial_info">
+                            <h5>Diana Ayi</h5>
+                            <small>Student</small>
+                            <div className="testimonial_body">
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsam consequuntur corrupti doloremque impedit minima nesciunt, architecto cum aspernatur animi eos totam veritatis iusto voluptatibus voluptatem voluptas dolor
+                                    dicta autem!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className="testimonial swiper-slide">
+                        <div className="avatar">
+                            <img src={avatar2} alt="" srcSet="" />
+                        </div>
+                        <div className="testimonial_info">
+                            <h5>Diana Ayi</h5>
+                            <small>Student</small>
+                            <div className="testimonial_body">
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsam consequuntur corrupti doloremque impedit minima nesciunt, architecto cum aspernatur animi eos totam veritatis iusto voluptatibus voluptatem voluptas dolor
+                                    dicta autem!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className="testimonial swiper-slide">
+                        <div className="avatar">
+                            <img src={avatar3} alt="" srcSet="" />
+                        </div>
+                        <div className="testimonial_info">
+                            <h5>Diana Ayi</h5>
+                            <small>Student</small>
+                            <div className="testimonial_body">
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsam consequuntur corrupti doloremque impedit minima nesciunt, architecto cum aspernatur animi eos totam veritatis iusto voluptatibus voluptatem voluptas dolor
+                                    dicta autem!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className="testimonial swiper-slide">
+                        <div className="avatar">
+                            <img src={avatar5} alt="" srcSet="" />
+                        </div>
+                        <div className="testimonial_info">
+                            <h5>Diana Ayi</h5>
+                            <small>Student</small>
+                            <div className="testimonial_body">
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsam consequuntur corrupti doloremque impedit minima nesciunt, architecto cum aspernatur animi eos totam veritatis iusto voluptatibus voluptatem voluptas dolor
+                                    dicta autem!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <article className="testimonial swiper-slide">
+                        <div className="avatar">
+                            <img src={avatar4} alt="" srcSet="" />
+                        </div>
+                        <div className="testimonial_info">
+                            <h5>Diana Ayi</h5>
+                            <small>Student</small>
+                            <div className="testimonial_body">
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsam consequuntur corrupti doloremque impedit minima nesciunt, architecto cum aspernatur animi eos totam veritatis iusto voluptatibus voluptatem voluptas dolor
+                                    dicta autem!
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </SwiperSlide>
+            </Swiper>
+            {/* <article className="testimonial swiper-slide">
                     <div className="avatar">
                         <img src={avatar1} alt="" srcSet="" />
                     </div>
@@ -89,8 +181,7 @@ const Testimonial = () => {
                             </p>
                         </div>
                     </div>
-                </article>
-            </div>
+                </article> */}
         </section>
     )
 }
