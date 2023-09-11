77d4137035fcda80f2ccee2eb40d56812c0d46ff
williamtanuwijaya <will.tw0807@gmail.com>
2023-09-11 10:58:39 +0700

    I fix the BUG in the navbar in file Navbar.jsx

--- src/sections/index/navbar/Navbar.jsx
+++ src/sections/index/navbar/Navbar.jsx
@@ -1,22 +1,49 @@
-import './navbar.css'
-import './../../../index.css'
-import data from './data'
+import { useState, useEffect } from 'react';
+import './navbar.css';
+import './../../../index.css';
+import data from './data';
+import Hamburger from 'hamburger-react';
 
 const Navbar = () => {
-    return (
-            <nav>
-                <div className="container nav_container">
-                    <a href="/"><h4>CampDev</h4></a>
-                    <ul className='nav_menu'>
-                        {
-                            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
-                        }
-                    </ul>
-                    <button id="open-menu-btn"><i data-feather="menu"></i></button>
-                    {/* <button id="close-menu-btn"><i data-feather="x"></i></button> */}
-                </div>
-            </nav>
-    )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const closeMenu = (event) => {
+      if (!event.target.closest('nav') && isMenuOpen) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => {
+      document.removeEventListener('click', closeMenu);
+    };
+  }, [isMenuOpen]);
+
+  return (
+    <nav>
+      <div className={`container nav_container ${isMenuOpen ? 'menu-open' : ''}`}>
+        <a href="/">
+          <h4>CampDev</h4>
+        </a>
+        <ul className={`nav_menu ${isMenuOpen ? 'active' : ''}`}>
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="open-menu-btn" onClick={toggleMenu}>
+          <Hamburger size={20} />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/sections/index/navbar/data.js
+++ src/sections/index/navbar/data.js
@@ -1,9 +1,9 @@
 const data = [
-  { id: 1, link: "./", title: 'Home' },
-  { id: 2, link: "./About", title: 'About' },
-  { id: 3, link: "./Course", title: 'Courses' },
-  { id: 4, link: "./Contact", title: 'Contact' },
-]
+  { id: 1, link: './', title: 'Home' },
+  { id: 2, link: './About', title: 'About' },
+  { id: 3, link: './Course', title: 'Courses' },
+  { id: 4, link: './Contact', title: 'Contact' },
+];
 
 // change navbar style on scroll
 
@@ -11,21 +11,24 @@ window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 });
 
-// Ambil elemen tombol menu
-const openMenuButton = document.getElementById("open-menu-btn");
-const navMenu = document.querySelector(".nav_menu");
+// // Ambil elemen tombol menu
+// // const openMenuButton = document.querySelectorAll('.open-menu-btn');
+// openMenuButton()=>{
+//   return document.querySelector('.open-menu-btn');
+// }
+// const navMenu = document.querySelector('.nav_menu');
 
-// Ketika tombol buka menu diklik
-// openMenuButton.addEventListener("click", () => {
-//   navMenu.classList.toggle("active");
+// // Ketika tombol buka menu diklik
+// openMenuButton.addEventListener('click', () => {
+//   navMenu.classList.toggle('active');
 // });
 
-// Menutup menu jika pengguna mengklik di luar menu
-document.addEventListener("click", (event) => {
-  const target = event.target;
-  if (!target.closest("nav") && navMenu.classList.contains("active")) {
-    navMenu.classList.remove("active");
-  }
-});
+// // Menutup menu jika pengguna mengklik di luar menu
+// document.addEventListener('click', (event) => {
+//   const target = event.target;
+//   if (!target.closest('nav') && navMenu.classList.contains('active')) {
+//     navMenu.classList.remove('active');
+//   }
+// });
 
 export default data;
