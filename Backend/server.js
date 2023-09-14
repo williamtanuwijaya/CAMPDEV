50c59fd80f3d1af7edbe6fd16a05fa5a00a80cc7
williamtanuwijaya <will.tw0807@gmail.com>
2023-09-14 13:25:25 +0700

    i make the connected database mysql from xampp

--- Backend/server.js
+++ Backend/server.js
@@ -15,9 +15,49 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'signup',
+  database: 'dbcampdev',
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('database connected');
+
+  // Untuk mendapatkan data
+  app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM tbuser';
+    db.query(sql, (err, result) => {
+      const users = JSON.parse(JSON.stringify(result));
+      // res.render('index', { users: users, title: 'DAFTAR MURID' });
+      console.table(users);
+    });
+
+    
+  });
+
+  // // Untuk menyimpan data
+  app.post('/tambah', (req, res) => {
+    const insertSQL = `INSERT INTO tbuser (id, nama, email, password_hash) VALUES ('4','tes','tes','tes')`;
+    db.query(insertSQL, (err, result) => {
+      if (err) throw err;
+      console.log("error");
+    });
+  });
 });
 
 app.listen(port, () => {
   console.log('Running ready..');
 });
+
+// app.get('/data', (req, res) => {
+//   const query = 'SELECT * FROM pengguna'; // Gantilah 'nama_tabel' dengan nama tabel Anda
+
+//   db.query(query, (err, results) => {
+//     console.log('results');
+//     if (err) {
+//       console.error('Error saat mengambil data:', err);
+//       res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data dari database' });
+//     } else {
+//       res.status(200).json(results);
+//     }
+//   });
+// });
