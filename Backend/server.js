64697071acf571671a8512752a52a2ac4ddf613c
williamtanuwijaya <will.tw0807@gmail.com>
2023-09-14 23:34:50 +0700

    I make the Restful API and then i make contact connect to EmailJS

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,11 @@ const db = mysql.createConnection({
   password: '',
   database: 'dbcampdev',
 });
-
+app.get('/', (req, res) => {
+  res.json({
+    test: 'halo',
+  });
+});
 db.connect((err) => {
   if (err) throw err;
   console.log('database connected');
@@ -31,41 +35,94 @@ db.connect((err) => {
     const sql = 'SELECT * FROM tbuser';
     db.query(sql, (err, result) => {
       const users = JSON.parse(JSON.stringify(result));
-      // res.render('index', { users: users, title: 'DAFTAR MURID' });
       console.table(users);
-      res.render('index');
+      res.json(users);
     });
   });
 
+  app.post('/sign-in', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const checkEmail = `SELECT * FROM tbuser WHERE email = '${email}' and password_hash = '${password}'`;
+    db.query(checkEmail, (req, result) => {
+      const resData = JSON.parse(JSON.stringify(result));
+      console.log(resData);
+      if (resData.length > 0) {
+        return res.json({
+          status: 'Sign in success',
+        });
+      } else {
+        return res.json({
+          status: 'sign in failed ',
+        });
+      }
+    });
+  });
   // // Untuk menyimpan data
-  app.post('/tambah', (req, res) => {
-    const nama = String(req.body.nama);
-    const email = String(req.body.email);
-    const pw = String(req.body.password_hash);
+  app.post('/sign-up', (req, res) => {
+    const nama = req.body.nama;
+    const email = req.body.email;
+    const pw = req.body.password_hash;
 
-    // const insertSQL = `INSERT INTO tbuser (id, nama, email, password_hash) VALUES (6,${nama},${email},'${pw}')`;
-    const insertSQL = `INSERT INTO tbuser (id, nama, email, password_hash) VALUES (10, '${nama}', '${email}', '${pw}')`;
+    const insertSQL = `INSERT INTO tbuser (nama, email, password_hash) VALUES ('${nama}', '${email}', '${pw}')`;
     db.query(insertSQL, (err, result) => {
       if (err) throw err;
-      console.log('error');
+      return res.json({
+        message: 'success',
+      });
+    });
+  });
+
+  // untuk mengambil data dari tabel message
+  app.get('/ambil-messange', (req, res) => {
+    const sql = 'SELECT * FROM tbmessage';
+    db.query(sql, (err, result) => {
+      const message = JSON.parse(JSON.stringify(result));
+      console.table(message);
+      res.json(message);
+    });
+  });
+
+  // untuk insert messange
+  app.post('/insert-messange', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const emailaddres = req.body.emailaddres;
+    const message = req.body.message;
+
+    const insertSQLCONTACT = `INSERT INTO tbmessage(firstname, lastname, emailadress, message) VALUES ('${firstname}', '${lastname}', '${emailaddres}', ${message})`;
+    db.query(insertSQLCONTACT, (err, result) => {
+      if (err) throw err;
+      return res.json({
+        message: 'success messange',
+      });
+    });
+  });
+
+  // untuk mengecek messange
+  app.post('/check-messange', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const emailaddres = req.body.emailaddres;
+    const message = req.body.message;
+    const checkMessange = `SELECT * FROM tbmessage WHERE firstname = '${firstname}' and lastname = '${lastname}' and emailadress = '${emailaddres}' and message = '${message}'`;
+
+    db.query(checkMessange, (req, result) => {
+      const resData = JSON.stringify(result);
+      console.log(resData);
+      if (Object.keys(resData.length > 0)) {
+        return res.json({
+          status: 'Messange success',
+        });
+      } else {
+        return res.json({
+          status: 'Messange failed ',
+        });
+      }
     });
   });
 });
 
 app.listen(port, () => {
-  console.log('Running ready..');
+  console.log(`Running ready.. at http://localhost:${port}`);
 });
-
-// app.get('/data', (req, res) => {
-//   const query = 'SELECT * FROM pengguna'; // Gantilah 'nama_tabel' dengan nama tabel Anda
-
-//   db.query(query, (err, results) => {
-//     console.log('results');
-//     if (err) {
-//       console.error('Error saat mengambil data:', err);
-//       res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data dari database' });
-//     } else {
-//       res.status(200).json(results);
-//     }
-//   });
-// });
