cbffa4f83fbf35762e40c3536dad0e71e28f11e5
williamtanuwijaya <will.tw0807@gmail.com>
2023-10-13 21:03:38 +0700

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Index, About, CoursePage, Contac } from './navigation/MainNavigation';
-import LoginRegister from './sections/login_register/LoginRegister';
-import { Dasboard } from './navigation/LogoutNavigation';
+import { About } from './navigation/MainNavigation';
+// import { Index, About, CoursePage, Contac } from './navigation/MainNavigation';
+// import LoginRegister from './sections/login_register/LoginRegister';
+// import { Dasboard } from './navigation/LogoutNavigation';
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<About />} />
         <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contac />} />
-        <Route path="/Course" element={<CoursePage />} />
-        <Route path='/dasboard' element={<Dasboard/>}/>
-        <Route path="/login_and_register" element={<LoginRegister/>}></Route>
+        <Route path="/Contact" element={<About />} />
+        <Route path="/Course" element={<About />} />
+        <Route path="/dasboard" element={<About />} />
+        <Route path="/login_and_register" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
