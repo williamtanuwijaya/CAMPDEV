fc4582f6aa27ca0a12b185dcd6a7acfc4121a35a
williamtanuwijaya <will.tw0807@gmail.com>
2023-10-13 21:15:37 +0700

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { About } from './navigation/MainNavigation';
-// import { Index, About, CoursePage, Contac } from './navigation/MainNavigation';
-// import LoginRegister from './sections/login_register/LoginRegister';
-// import { Dasboard } from './navigation/LogoutNavigation';
+import { Index, About, CoursePage, Contac } from './navigation/MainNavigation';
+import LoginRegister from './sections/login_register/LoginRegister';
+import { Dasboard } from './navigation/LogoutNavigation';
 
 function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<Index />} />
         <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<About />} />
-        <Route path="/Course" element={<About />} />
-        <Route path="/dasboard" element={<About />} />
-        <Route path="/login_and_register" element={<About />} />
+        <Route path="/Contact" element={<Contac />} />
+        <Route path="/Course" element={<CoursePage />} />
+        <Route path="/dasboard" element={<Dasboard />} />
+        <Route path="/login_and_register" element={<LoginRegister />} />
       </Routes>
     </BrowserRouter>
   );
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Specify the public path for your assets (change '/your-app/' to your actual subpath)
+    base: '/',
+  },
 })
